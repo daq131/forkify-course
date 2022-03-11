24a63027576d87b7df61cb946662dd79eaff98d3
daq131 <daq131@gmail.com>
2022-03-11 05:33:01 +0100

    initial commit

---
+++ src/js/config.js
@@ -0,0 +1,10 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const API_SRC = 'https://forkify-api.herokuapp.com/api/';
+
+export const Timeout_SEC = 10;
+export const RES_PER_PAGE = 10;
+
+export const KEY = 'd71c5bba-99d3-475d-a12a-9b1cc558405d';
+export const MODAL_CLOSE_SEC = 2.5;
+
+//https://forkify-api.herokuapp.com/api/v2/recipes/
